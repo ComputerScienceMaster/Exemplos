49616dfbc9be51d9cfe9531d0a7ab9697ea3cfb8
Vinicius dos Santos <vinistos@gmail.com>
2019-03-15 13:37:25 -0300

    Spyder webcrowler

---
+++ Python/Crowler scrappy/stackOverflowCrowler/stackOverflowCrowler/spiders/sp1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from stackOverflowCrowler.items import StackoverflowcrowlerItem
+
+class Sp1Spider(scrapy.Spider):
+    name = 'sp1'
+    allowed_domains = ['stackoverflow.com']
+    start_urls = ['https://stackoverflow.com/']
+    
+    def start_requests(self):
+        yield scrapy.Request('https://stackoverflow.com/questions/11827176/', self.parse)
+        yield scrapy.Request('https://stackoverflow.com/questions/11827175/', self.parse)
+        yield scrapy.Request('https://stackoverflow.com/questions/11827174/', self.parse)
+
+    def parse(self, response):
+        for h1 in response.xpath('//*[@id="question-header"]/h1/a').getall():
+            yield StackoverflowcrowlerItem(titulo=h1)
+
+        for href in response.xpath('//*[@id="question-header"]/h1/a').getall():
+            yield scrapy.Request(response.urljoin(href), self.parse)
