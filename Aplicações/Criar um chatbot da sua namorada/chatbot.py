dd9f1117090cb6085d0c21eddc4e6cb4dbbf130d
vinnydsstos <vinistos@gmail.com>
2021-09-08 09:29:13 -0300

    Atualizando chatbot

--- Aplicações/Criar um chatbot da sua namorada/chatbot.py
+++ Aplicações/Criar um chatbot da sua namorada/chatbot.py
@@ -1,48 +1,25 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 25 19:10:00 2021
-
-@author: Vinicius
-"""
 
 import re
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 
 corpus = open(r"conversa.txt", encoding="utf-8").read()
 
 preprocessedCorpus = re.sub('[0-9]+/[0-9]+/[0-9]+\s[0-9]+:[0-9]+\s-\s\w+:\s+', '', corpus)
-
 preprocessedCorpus = re.sub('\[[0-9]+/[0-9]+\s[0-9]+:[0-9]+\]\s+.*', '', preprocessedCorpus)
-
-
 preprocessedCorpus = re.sub('\[[0-9]+[0-9]+,\s[0-9]+/[0-9]+/[0-9]+\]\s+.*', '', preprocessedCorpus)
-
-
 preprocessedCorpus = re.sub('<Arquivo de mídia oculto>', '', preprocessedCorpus)
-
-
 preprocessedCorpus = re.sub('\s+\n', '', preprocessedCorpus)
 
-listOfSentences = preprocessedCorpus.split("\n")
-
-from chatbot import chatbot
-from chatterbot.trainers import ListTrainer
-
+chatbot = ChatBot("Vanessa")
 
 trainer = ListTrainer(chatbot)
+trainer.train(preprocessedCorpus)
 
-'''
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
-
-chatbot = ChatBot('Ron Obvious')
-
-# Create a new trainer for the chatbot
-trainer = ChatterBotCorpusTrainer(chatbot)
-
-# Train the chatbot based on the english corpus
-trainer.train("chatterbot.corpus.english")
-
-# Get a response to an input statement
-chatbot.get_response("Hello, how are you today?")
-'''
+while True:
+    request = input('Você: ')
+    response = chatbot.get_response(request)
+    print('Vanessa bot: ', response)
+    
