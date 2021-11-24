966d9d7dcf3df5df2664a403f251b099f02baaab
vinnydsstos <vinistos@gmail.com>
2021-11-24 20:22:50 -0300

    Caixeiro viajante em força bruta

--- Estruturas de dados/Grafos/Caixeiro Viajante/caixeiro_viajante.py
+++ Estruturas de dados/Grafos/Caixeiro Viajante/caixeiro_viajante.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 24 14:49:47 2021
-
-@author: Vinicius
+Criação das Classes que vão representar o grafo
 """
-
 class Node:
     vertex = 0
     weight = 0
@@ -19,6 +16,10 @@ class Edge:
 class Graph:
     edges = []
     
+
+"""
+Criação de um grafo - função que cria um grafo e inicializa os edges
+"""
     
 def createGraph(v):
     g = Graph()
@@ -26,12 +27,18 @@ def createGraph(v):
         g.edges.append(Edge(i))
     return g
 
+"""
+Cria um nó
+"""
 def createNode(v, weight):
     n = Node()
     n.vertex = v
     n.weight = weight
     return n
 
+"""
+Cria uma nova aresta
+"""
 def createEdge(g, vi, vf, w):
     if g is None:
         return False
@@ -46,16 +53,75 @@ def createEdge(g, vi, vf, w):
     else:
         g.edges[vi].nodes.append(newNode)
     
+"""
+Mostra o grafo
+"""
 def printGraph(g):
     for i in range (0, len(g.edges)):
         print ("v" + str(i), end=" →  ")
         for nodes in g.edges[i].nodes:
             print ("v" + str(nodes.vertex) + "(" + str(nodes.weight) + ")" , end="|")
         print("\n")
+        
+"""
+Valida uma rota
+"""
+def validarRota(g, rota):
+    ## verifica se a rota repete algum valor
+    a_set = set(rota)
+    contains_duplicates = len(rota) != len(a_set)
+    if contains_duplicates == True:
+
+        return False
+    
+    ## verifica se a rota possui caminhos para percorrer as adjacencias
+    for r in range (0,len(rota) -1):
+        vi = rota[r]
+        vf = rota[r + 1]
+        checked = False
+        for n in g.edges[vi].nodes:
+            if n.vertex == vf:
+                checked = True
+        if checked == False:
+            return False
+    return True
+                 
+"""
+Soma pesos
+"""
+def somaPesos(g, rota):
+    ## Soma os pesos da rota
+    soma = 0
+    for r in range (0,len(rota) -1):
+        vi = rota[r]
+        vf = rota[r + 1]
+        checked = False
+        for n in g.edges[vi].nodes:
+             if n.vertex == vf:
+                soma += n.weight
+                checked = True
+        if checked == False:
+            return False
+    return soma
+
+"""
+Resolução de força bruta
+"""
+def resolverComForcaBruta():
+    ## porquê não aplicar um pouco de força bruta?
+    for a in range(0,4):
+        for b in range(0,4):
+            for c in range(0,4):
+                for d in range(0,4):
+                    rota = [a,b,c,d]
+                    if(validarRota(g,rota) == True):
+                        print("------------")
+                        print(rota)
+                        print(validarRota(g, rota))                
+                        print(somaPesos(g,rota))
+                        print("------------")
+    
             
-
-
-
 g = createGraph(4)
 createEdge(g, 0,2,4)
 createEdge(g, 0,1,10)
@@ -64,4 +130,5 @@ createEdge(g, 2,3,21)
 createEdge(g, 3,1,32)
 createEdge(g, 4,2,14)
 
+resolverComForcaBruta()
 printGraph(g)
